c592170372e9929240e699041a68ba65c1ebca21
Alex Schultz <aschultz@mirantis.com>
2015-12-15 13:39:30 -0700

    Fixed missing quote

--- deployment_scripts/commit_etckeeper.pp
+++ deployment_scripts/commit_etckeeper.pp
@@ -2,5 +2,5 @@ notice('MODULAR: fuel-plugin-etckeeper/commit_etckeeper.pp')
 
 exec { 'etckeeper-commit':
   path    => '/bin:/usr/bin:/usr/local/bin',
-  command => '/usr/bin/etckeeper commit "Deployment End',
+  command => '/usr/bin/etckeeper commit "Deployment End"',
 }
